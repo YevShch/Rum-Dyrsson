8112d3c9d3883f62c3f1444cdc4bb72ae5d37260
YevShch <yevheniiashcherbakova82@gmail.com>
2023-12-05 10:17:41 +0100

    upd

---
+++ public/login-Back.js
@@ -0,0 +1,11 @@
+import { validLogin } from "./check.js"
+
+
+$( "#saveHouse" ).on('click', function () {
+  const username = $( '#username' ).val();
+  const password = $( '#password' ).val();
+  if ( validLogin( username, password ) ) {
+    return true
+  }
+  return ( "det är fel på användarnamnet eller lösenordet" )
+} )
