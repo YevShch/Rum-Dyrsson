cf31175ddd162037d65c66f32f78b18e78c5f615
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2023-12-14 19:52:57 +0100

    commit

--- public/pages/buy.js
+++ public/pages/buy.js
@@ -1,118 +1,72 @@
 import { getAll } from "../server-request.js";
 
-export default function buy () {
-  return $( `
+export default async function buy() {
+  return $(`
    <div id="showResidence">
-   <section id="houseShow">
-   <h1>Hitta ditt drömhus:</h1>
+   <h2>Alla bostäder:</h2>
    
-  
+      <div class="filterResidence">
+        <label for="sortOrder">Sortera efter:</label>
+        <select id="sortOrder">
+          <option value="priceAsc">Pris (Lägst överst)</option>
+          <option value="priceDesc">Pris (Högst överst)</option>
+        </select>
+
+        <label for="residenceType">Bostadstyp:</label>
+        <select id="residenceType">
+          <option value="all">Alla</option>
+          <option value="Villa">Villa</option>
+          <option value="Radhus">Radhus</option>
+          <option value="Lägenhet">Lägenhet</option>
+          <option value="Tomt">Tomt</option>
+        </select>
+
+        <button onclick="filterResidences()">Filtrera</button>
+      </div>
   <div>
-  <form id="bostad">
-
-    <section class="FiltreraBostad">
-    <fieldset>
-    <legend>Filtrera sökning:</legend>
-    <div>
-      <input type="radio" id="Hus" name="väljBostad" value="Hus" />
-      <label for="Hus">Hus</label>
-
-      <input type="radio" id="Lägenhet" name="väljBostad" value="Lägenhet" />
-      <label for="Lägenhet">Lägenhet</label>
-
-      <input type="radio" id="Villa" name="väljBostad" value="Villa" />
-      <label for="Villa">Villa</label>
-      <br><br>
-
-
-
-<label for="price">Utgångspis:</label>
-<input type="number" id="price" name="price" min="50000" max="999000000" value="50000" step="10000" title="Pris ska vara sifror"/>
-<br>
-
-<label for="rooms">Antal rums:</label>
-<input id="rooms" type="number" name="rooms" min="1" max="20" value="1" required/>
-<br>
-
-<label for="area">Boarea, kvm:</label>
-<input id="area" type="number" name="area"  min="20" max="500" value="20" step = "5" required/>
-<br>
-
-
-</fieldset>
-</section>
-
-</form>
-<form id="getAllBuy">
-<button id="getAllBuy" type="submit">Visa alla bostäder till salu</button>
-</form>
-</section>
   `);
 }
 
-export function getAllResidence () {
+function filterResidences() {
 
-  $( "#getAllBuy" ).on( "submit", function ( event ) {
-    event.preventDefault()
-    console.log( "Knappen fungerar!" );
-    getAllBuyData()
-  }
-  );
 }
 
-async function getAllBuyData () {
-  const bostad = await getAll( "buy" );
-  console.log( bostad )
-  for ( let i = 0; i < bostad.length; i++ ) {
-    const section = $( `
-       <div id="houseSort">
-       <form id="showBuyList">
+export async function getAllResidence() {
+  $(document).ready(async function () {
+    const bostad = await getAll("buy");
+    console.log(bostad)
+    for (let i = 0; i < bostad.length; i++) {
+      const section = $(`
+       <div id="sort">
+       <form id="showResidence">
        <fieldset>
          <form id="showDetail">
-        <h2>${ bostad[ i ].address }</h2><h2>${ bostad[ i ].city }</h2>
-       <img src="${ bostad[ i ].photo }" alt="beskrivning_av_bilden"style="width:200px;height:200px;cursor:pointer;">
-         <p>Typ av bostad: ${ bostad[ i ].type }</p>
-         <p>Pris: ${ bostad[ i ].price }</p>
-         <p>Antal rum: ${ bostad[ i ].rooms }</p>
-         <p>Boarea: ${ bostad[ i ].area }</p>
+        <h2>${bostad[i].address}</h2><h2>${bostad[i].city}</h2>
+       <img src="${bostad[i].photo}" alt="beskrivning_av_bilden"style="width:200px;height:200px;cursor:pointer;">
+         <p>Typ av bostad: ${bostad[i].type}</p>
+         <p>Pris: ${bostad[i].price}</p>
+         <p>Antal rum: ${bostad[i].rooms}</p>
+         <p>Boarea: ${bostad[i].area}</p>
         
         <button type="submit" id="showDetail">Visa bostad</button>
+
+        <button class="action-button" data-action="delete" value=${bostad[i].id}>Hämta ID</button> 
         </form>
         </fieldset>
       </form>
       </div>
-      <script>
-      
-      </script>
     `);
-    $( "#showDetail" ).click( function () {
-      // deleteOne( "buy", buyID )
-      // console.log( data )
-      console.log( "Knappen funkar!" );
-    } )
-
-    $( "#showResidence" ).append( section );
-  }
+      $("#showResidence").append(section);
+    }
+  })
 }
 
-$( document ).ready( function () {
-  $( 'form#showBuyList' ).on( 'click', 'img', function () {
-    $( this ).closest( 'form#showBuyList' ).find( 'img' ).not( this ).hide();
-    $( this ).closest( 'form#showBuyList' ).find( 'p' ).not( $( this ).next() ).hide();
-  } );
-} );
+export function addBuyEventListeners() {
+  $('#showResidence').on('click', '.action-button', function () {
+    console.log("Knappen fungerar!");
+    const id = $(this).val();
+    const action = $(this).data('action');
+    console.log(`Button clicked with id: ${id} and action: ${action}`);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  });
+}
--- public/router.js
+++ public/router.js
@@ -1,5 +1,5 @@
 import sell, { addSellsEventlistner } from "./pages/sell.js"
-import buy, { getAllResidence }  from "./pages/buy.js"
+import buy, { getAllResidence, addBuyEventListeners } from "./pages/buy.js"
 import realters from "./pages/realters.js"
 import login, { addLoginsEventlistner } from "./pages/login.js"
 import admin, { addLogoutEventlistner } from "./pages/admin.js"
@@ -14,9 +14,9 @@ async function router() {
       addSellsEventlistner()
       break
     case "#buy":
-      $( 'main' ).html( await buy() )
+      $('main').html(await buy())
       getAllResidence()
-      // addBuyEventListeners()
+      addBuyEventListeners()
       break
     case "#realters":
       $('main').html(realters())
