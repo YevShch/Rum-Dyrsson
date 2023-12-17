bc6aff9d8a980d433cbf3b2168acfc1ef7f5802b
JoakimChan <120197963+JoakimChan@users.noreply.github.com>
2023-12-17 17:50:40 +0100

    commit

--- public/pages/sellList.js
+++ public/pages/sellList.js
@@ -1,107 +1,107 @@
 import { getAll, getOne, addOne, deleteOne } from "../server-request.js";
 
-export default async function sellList () {
-  if ( await checkLogIn() ) {
+export default async function sellList() {
+  if (await checkLogIn()) {
     window.location.href = "#login";
   }
-  return $( `
+  return $(`
    <div id="sellList">
   <div>
   `);
 }
 
-export async function getAllSellData () {
-  $( document ).ready( async function () {
-    const bostad = await getAll( "sell" );
-    console.log( bostad )
-    const quantitySell = $( `
-    <h3>Du har ${ bostad.length } sälförfrågningar</h3>
+export async function getAllSellData() {
+  $(document).ready(async function () {
+    const bostad = await getAll("sell");
+    console.log(bostad)
+    const quantitySell = $(`
+    <h3>Du har ${bostad.length} sälförfrågningar</h3>
    `);
-    $( "#sellList" ).append( quantitySell );
-    for ( let i = 0; i < bostad.length; i++ ) {
-      const section = $( `
+    $("#sellList").append(quantitySell);
+    for (let i = 0; i < bostad.length; i++) {
+      const section = $(`
       <fieldset>
       <form id="showSellList">
        
-       <p><b><u>${ bostad[ i ].id }</p></b></u>
-        <p>Foto: ${ bostad[ i ].photo }</p>
-         <p>Typ av bostad: ${ bostad[ i ].type }</p>
-         <p>Gatuadress: ${ bostad[ i ].address }</p> 
-         <p>Ort: ${ bostad[ i ].city }</p>
-         <p>Pris: ${ bostad[ i ].price }</p>
-         <p>Antal rum: ${ bostad[ i ].rooms }</p>
-         <p>Boarea: ${ bostad[ i ].area }</p>
-         <p>Balkong: ${ bostad[ i ].balcony }</p>
-         <p>Våningen: ${ bostad[ i ].floor }</p>
-         <p>Hiss: ${ bostad[ i ].elevator }</p>
-         <p>Byggnadsår: ${ bostad[ i ].year }</p>
-         <p>Förråd: ${ bostad[ i ].storehouse }</p>
-         <p>Parkeringplats: ${ bostad[ i ].parking }</p>
-         <p>Innegård: ${ bostad[ i ].garden }</p>
-         <p>Namn: ${ bostad[ i ].firstName }</p>
-         <p>Efternamn: ${ bostad[ i ].lastName }</p>
-         <p>Mejladress: ${ bostad[ i ].email }</p>
-         <p>Telefonnummer: ${ bostad[ i ].phone }</p>
+       <p><b><u>${bostad[i].id}</p></b></u>
+        <p>Foto: ${bostad[i].photo}</p>
+         <p>Typ av bostad: ${bostad[i].type}</p>
+         <p>Gatuadress: ${bostad[i].address}</p> 
+         <p>Ort: ${bostad[i].city}</p>
+         <p>Pris: ${bostad[i].price}</p>
+         <p>Antal rum: ${bostad[i].rooms}</p>
+         <p>Boarea: ${bostad[i].area}</p>
+         <p>Balkong: ${bostad[i].balcony}</p>
+         <p>Våningen: ${bostad[i].floor}</p>
+         <p>Hiss: ${bostad[i].elevator}</p>
+         <p>Byggnadsår: ${bostad[i].year}</p>
+         <p>Förråd: ${bostad[i].storehouse}</p>
+         <p>Parkeringplats: ${bostad[i].parking}</p>
+         <p>Innegård: ${bostad[i].garden}</p>
+         <p>Namn: ${bostad[i].firstName}</p>
+         <p>Efternamn: ${bostad[i].lastName}</p>
+         <p>Mejladress: ${bostad[i].email}</p>
+         <p>Telefonnummer: ${bostad[i].phone}</p>
 
-        <button class="action-button" data-action="publish" value=${ bostad[ i ].id }>Publicera</button> 
+        <button class="action-button" data-action="publish" value=${bostad[i].id}>Publicera</button> 
      
-        <button class="action-button" data-action="delete" value=${ bostad[ i ].id }>Ta bort</button> 
+        <button class="action-button" data-action="delete" value=${bostad[i].id}>Ta bort</button> 
    
       </form>
       </fieldset>
     `);
 
-      $( "#sellList" ).append( section );
+      $("#sellList").append(section);
     }
-  } )
+  })
 }
 
-export function sellButtonEventListeners () {
-  $( '#sellList' ).on( 'click', '.action-button', function () {
-    const id = $( this ).val();
-    const action = $( this ).data( 'action' );
-    console.log( `Button clicked with id: ${ id } and action: ${ action }` );
+export function sellButtonEventListeners() {
+  $('#sellList').on('click', '.action-button', function () {
+    const id = $(this).val();
+    const action = $(this).data('action');
+    console.log(`Button clicked with id: ${id} and action: ${action}`);
 
     // Execute the async logic based on the button click
-    if ( action === 'publish' ) {
-      publish( id );
-    } else if ( action === 'delete' ) {
-      remove( id );
+    if (action === 'publish') {
+      publish(id);
+    } else if (action === 'delete') {
+      remove(id);
     }
-    console.log( "end" )
-  } );
+    console.log("end")
+  });
 }
 
 // Async functions
-export async function publish ( id ) {
+export async function publish(id) {
   try {
-    const data = await getOne( "sell", id )
-    await addOne( "buy", data )
-    await deleteOne( "sell", id )
-  } catch ( error ) {
-    console.error( 'Error in publish:', error );
+    const data = await getOne("sell", id)
+    await addOne("buy", data)
+    await deleteOne("sell", id)
+  } catch (error) {
+    console.error('Error in publish:', error);
   }
   window.location.reload()
 }
 
-export async function remove ( id ) {
+export async function remove(id) {
   try {
-    await deleteOne( "sell", id );
-    console.log( "Complete" );
-  } catch ( error ) {
-    console.error( 'Error in remove:', error );
+    await deleteOne("sell", id);
+    console.log("Complete");
+  } catch (error) {
+    console.error('Error in remove:', error);
   }
   window.location.reload()
 }
 
-async function checkLogIn () {
-  const user = await getOne( "admin", 1 )
-  console.log( user.logIn )
-  if ( user.logIn === 0 ) {
-    console.log( "true" )
+async function checkLogIn() {
+  const user = await getOne("admin", 1)
+  console.log(user.logIn)
+  if (user.logIn === 0) {
+    console.log("true")
     return true
   } else {
-    console.log( "false" )
+    console.log("false")
     return false
   }
 }
